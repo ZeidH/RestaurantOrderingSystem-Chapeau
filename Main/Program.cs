3e4e2a51553c5e8465aaba326a1681e5d78c97be
Plez <615284@student.inholland.nl>
2018-05-08 21:25:12 +0200

    Added anti flicker

--- Main/Program.cs
+++ Main/Program.cs
@@ -16,7 +16,7 @@ namespace Main
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Chapeau_Form());
         }
     }
 }
