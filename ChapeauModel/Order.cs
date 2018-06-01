75a540fa452964326a65ff6a4aee6c4a5d89586e
D <danuta.lapina@outlook.com>
2018-06-01 21:38:00 +0200

    Add models for kitchen and bar order view

---
+++ ChapeauModel/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ChapeauModel
+{
+	public class Order
+	{
+		List<OrderItem> Items { get; set; }
+
+		OrderStatus Status
+		{
+			get
+			{
+				// todo: combine order statuses of all the items
+				return OrderStatus.Ready;
+			}
+		}
+	}
+
+
+	//public float Tip { get; set; }
+	//public PayMethod PaymentMethod { get; set; }
+}
